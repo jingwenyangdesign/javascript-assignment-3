9384831ee8f76fe3e6bf336213b5e06c5e074804
Jingwen Yang <jyang77@id.iit.edu>
2017-04-17 15:57:17 -0500

    add ray, mountaintop, mountaincap

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Do your work in this file.
 
-
 function addDivView() {
     var DivView = document.createElement("div");
     DivView.className = "view";
@@ -10,29 +9,46 @@ function addDivView() {
 function addDivGrass() {
     var DivGrass = document.createElement("div");
     DivGrass.className = "grass";
-    document.body.appendChild(DivGrass);
     document.body.getElementsByClassName("view")[0].appendChild(DivGrass);
 }
 
 function addDivSun() {
     var DivSun = document.createElement("div");
     DivSun.className = "sun";
-    document.body.appendChild(DivSun);
     document.body.getElementsByClassName("view")[0].appendChild(DivSun);
+}
+
+addDivView();
+addDivGrass();
+addDivSun();
+
+var t = 1;
+while (t < 6) {
+    var DivRay = document.createElement("div");
+    DivRay.className = "ray-" + t;
+    document.body.getElementsByClassName("sun")[0].appendChild(DivRay);
+    t++;
 }
 
 function addDivMountain() {
     var DivMountain = document.createElement("div");
     DivMountain.className = "mountain";
-    document.body.appendChild(DivMountain);
     document.body.getElementsByClassName("view")[0].appendChild(DivMountain);
 }
 
-// for (var t = 0; t < 5; t++) {
-//     addDivRay();
-// }
+function addDivMountaintop() {
+    var DivMountaintop = document.createElement("div");
+    DivMountaintop.className = "mountain-top";
+    document.body.getElementsByClassName("mountain")[0].appendChild(DivMountaintop);
+}
 
-addDivView()
-addDivGrass()
-addDivSun()
-addDivMountain()
+addDivMountain();
+addDivMountaintop();
+
+var m = 1;
+do {
+    var DivMountaincap = document.createElement("div");
+    DivMountaincap.className = "mountain-cap-" + m;
+    document.body.getElementsByClassName("mountain-top")[0].appendChild(DivMountaincap);
+    m++;
+} while (m < 4);
